95e200c792d0f7e662b538e2c8ec3169d6121516
kendypham <kendypham050197@gmail.com>
2018-07-30 12:55:13 +0700

    chuyen qua file js

--- 3/myscript.js
+++ 3/myscript.js
@@ -1,11 +1,40 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginRight = "300px";
-    setTimeout(closeNav,3000);
-    clearTimeout(openNav);
+    document.getElementById("mySidenav").style.width = "520px";
+    document.getElementById("ninja-slider").style.marginLeft = "20px";
+    // setTimeout(closeNav,3000);
+    // clearTimeout(openNav);
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("ninja-slider").style.marginLeft = "50px";
+}
+
+function choose(){
+    var x = document.getElementsByClassName("choosen");
+    for (var i = 0; i < x.length; i++) {
+        x[i].style.border = "1px solid black";
+
+    }
+}
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
 }
