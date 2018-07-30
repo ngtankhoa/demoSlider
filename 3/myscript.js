b6e30905f61045d5f13f63b5b75885bb8a5eef75
kendypham <kendypham050197@gmail.com>
2018-07-30 21:18:31 +0700

    thay doi hinh khi click chuot

--- 3/myscript.js
+++ 3/myscript.js
@@ -9,13 +9,15 @@ function closeNav() {
     document.getElementById("ninja-slider").style.marginLeft = "50px";
 }
 
-function choose(){
-    var x = document.getElementsByClassName("choosen");
-    for (var i = 0; i < x.length; i++) {
-        x[i].style.border = "1px solid black";
-
-    }
-}
+// function choose(){
+//     var x = document.getElementsByClassName("choosen");
+//     console.log(x);
+//     for (var i = 0; i < x.length; i++) {
+//         console.log(x[i]);
+//         x[i].style.border = "1px solid black";
+//         return false;
+//     }
+// }
 
 
 
