b7cfc1199d20c772be4348a9e80d49cc8a4ddbd3
kendypham <kendypham050197@gmail.com>
2018-07-30 21:25:41 +0700

    click chuyen hinh sidebar

--- 3/myscript.js
+++ 3/myscript.js
@@ -9,15 +9,7 @@ function closeNav() {
     document.getElementById("ninja-slider").style.marginLeft = "50px";
 }
 
-// function choose(){
-//     var x = document.getElementsByClassName("choosen");
-//     console.log(x);
-//     for (var i = 0; i < x.length; i++) {
-//         console.log(x[i]);
-//         x[i].style.border = "1px solid black";
-//         return false;
-//     }
-// }
+
 
 
 
