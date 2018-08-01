4933f37f7e60df9dc9e1ece60de44e1d6878586d
ngtankhoa <khoant.uit@gmail.com>
2018-08-01 11:52:25 +0700

    mirror fix

--- 3/myscript.js
+++ 3/myscript.js
@@ -1,16 +1,12 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "520px";
     document.getElementById("ninja-slider").style.marginLeft = "20px";
-    document.getElementById("thumbnail-slider").style.marginLeft = "-60px";
-    document.getElementsByClassName("changeLanguage").style.marginLeft = "-60px";
-    setTimeout(closeNav,30000);
+    setTimeout(closeNav, 30000);
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("ninja-slider").style.marginLeft = "75px";
-    document.getElementById("thumbnail-slider").style.marginLeft = "-25px";
-    document.getElementsByClassName("changeLanguage").style.marginLeft = "-25px";
 }
 
 
