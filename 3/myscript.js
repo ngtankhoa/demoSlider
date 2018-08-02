4ed49f21cb415c5ce148403425bf98c100e70aa7
ngtankhoa <khoant.uit@gmail.com>
2018-08-02 16:44:10 +0700

    testresUI

--- 3/myscript.js
+++ 3/myscript.js
@@ -1,7 +1,7 @@
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("col").style.marginRight="0%";
+    document.getElementsByClassName("row")[0].style.width = "100%";
 }
 
 
